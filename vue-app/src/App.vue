46c2c674e19267ff9804b9efe0e69f7f33327eb2
Barney Davis <barneydmedia@gmail.com>
2018-02-18 20:53:10 -0500

    Add working vue app

---
+++ vue-app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Giphy/>
+  </div>
+</template>
+
+<script>
+import Giphy from './components/Giphy'
+
+export default {
+  name: 'App',
+  components: {
+    Giphy
+  }
+}
+</script>
